5352239f5a4ba4f976b741aece5164c27b724c68
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2020-10-27 19:57:35 +0700

    simplify

--- binarClass/src/controllers/PostController/index.js
+++ binarClass/src/controllers/PostController/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable radix */
-/* eslint-disable eqeqeq */
 import fs from 'fs'
 import path from 'path'
 import data from '../../data/data.json'
@@ -8,30 +6,32 @@ const filePath = path.resolve(__dirname, '../../data/data.json')
 
 class PostController {
   static get = (req, res) => {
-    // Sort by Id (Ascending)
-    data.sort((a, b) => parseInt(a.id) - parseInt(b.id));
+    let filteredData = data
+    const { title, sort } = req.query
 
-    // Search by title
-    const { title } = req.query;
-    const byTitle = data.filter((query) => query.title.toLowerCase() == title)
-    if (title) {
-      return res.status(200).json(byTitle);
+    // Sort
+    if (sort) {
+      filteredData.sort((a, b) => parseInt(a.id, 10) - parseInt(b.id, 10))
+
+      if (sort === 'desc') {
+        filteredData.reverse()
+      }
     }
 
-    return res.status(200).json(data);
+    // Search by title
+    if (title) {
+      filteredData = filteredData.filter((elm) => elm.title.toLowerCase().includes(title))
+    }
+
+    return res.status(200).json({ data: filteredData })
   }
 
   static add = (req, res) => {
-    const idCheck = data.find((index) => index.id === req.body.id)
+    const { id } = req.body
+    const post = data.find((obj) => obj.id === parseInt(id, 10))
 
-    // Check if any req body fields are missing
-    if (!req.body.id || !req.body.title || !req.body.author) {
-      return res.status(401).send({ error: true, message: 'Field missing in request body.' })
-    }
-
-    // Check if id is exist already
-    if (idCheck) {
-      return res.status(400).send({ error: true, message: 'Bad Request! Id is exist already.' })
+    if (post) {
+      return res.status(400).json({ message: 'id is already exist' })
     }
 
     data.push(req.body)
@@ -40,53 +40,61 @@ class PostController {
       filePath,
       JSON.stringify(data),
       'utf-8',
-      () => res.status(201).json({ message: `Successfully saved new data with id:${req.body.id} on ${filePath}` }),
+      () => res.status(201).json({ message: `Successfully saved on ${filePath}` }),
     )
   }
 
   static update = (req, res) => {
-    const idCheck = data.find((index) => index.id == req.params.id)
+    const { id } = req.params
+    const post = data.find((obj) => obj.id === parseInt(id, 10))
 
-    // Check if id isn't exist already
-    if (!idCheck) {
-      return res.status(404).send({ error: true, message: '404! Id is not exist.' })
+    if (!post) {
+      return res.status(404).json({ message: 'Not Found' })
     }
 
-    for (let i = 0; i < data.length; i += 1) {
-      if (data[i].id == req.params.id) {
-        data[i].id = req.body.id;
-        data[i].title = req.body.title;
-        data[i].author = req.body.author;
-      }
-    }
+    // Alternative 1
+    const { title, author } = req.body
+    post.title = title || post.title
+    post.author = author || post.author
+
+    // Alternative 2
+    // Object.keys(req.body).forEach((key) => {
+    //   if (req.body[key]) {
+    //     post[key] = req.body[key]
+    //   }
+    // })
 
     return fs.writeFile(
       filePath,
       JSON.stringify(data),
       'utf-8',
-      () => res.status(200).json({ error: true, message: `Successfully saved data update on ${filePath}` }),
+      () => res.status(200).json({ success: true, message: 'data updated', data: req.body }),
     )
   }
 
   static delete = (req, res) => {
-    const idCheck = data.find((index) => index.id == req.params.id)
+    const { id } = req.params
+    const post = data.find((obj) => obj.id === parseInt(id, 10))
 
-    // Check if id isn't exist already
-    if (!idCheck) {
-      return res.status(404).send({ error: true, message: '404! Id is not exist.' })
+    if (!post) {
+      return res.status(404).json({ message: '404! Id is not exist.' })
     }
 
+    // Alternative 1
     for (let i = 0; i < data.length; i += 1) {
-      if (data[i].id == req.params.id) {
-        data.splice(i, 1);
+      if (data[i].id === parseInt(id, 10)) {
+        data.splice(i, 1)
       }
     }
+
+    // Alternative 2
+    // data.splice(data.indexOf(post), 1)
 
     return fs.writeFile(
       filePath,
       JSON.stringify(data),
       'utf-8',
-      () => res.status(200).json({ message: `Successfully delete data with id:${req.params.id} on ${filePath}` }),
+      () => res.status(200).json({ message: `Successfully delete data with id:${id} on ${filePath}` }),
     )
   }
 }
